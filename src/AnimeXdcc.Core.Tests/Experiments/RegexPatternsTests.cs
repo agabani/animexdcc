ac41ce02437dbf622397dd3ec0ad67cc444bd924
Ahmed Agabani <agabani@live.com>
2015-08-09 19:25:03 +0100

    Added experimental regex tests

---
+++ src/AnimeXdcc.Core.Tests/Experiments/RegexPatternsTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using NUnit.Framework;
+
+namespace AnimeXdcc.Core.Tests.Experiments
+{
+    [TestFixture]
+    public class RegexPatternsTests
+    {
+        [Test]
+        [TestCase(@"[HorribleSubs] Fate Stay Night - Unlimited Blade Works - 01v2 [1080p].mkv", "01", "2")]
+        [TestCase(@"[HorribleSubs] Fate Stay Night - Unlimited Blade Works - 11 [1080p].mkv", "11", null)]
+        public void HorribleSubs_Fate_Stay_Night_Unlimited_Blade_Works(string input, string episode, string version)
+        {
+            var match = Regex.Match(input,
+                @"\[HorribleSubs\] Fate Stay Night - Unlimited Blade Works - (\d+)v?(\d+)? \[1080p\].mkv",
+                RegexOptions.IgnoreCase);
+
+            Assert.That(match.Success, Is.True);
+
+            foreach (var @group in match.Groups)
+            {
+                Console.WriteLine(@group);
+            }
+
+            Assert.That(match.Groups.Count, Is.EqualTo(2).Or.EqualTo(3));
+            Assert.That(match.Groups[1].Value, Is.EqualTo(episode));
+            if (version != null)
+            {
+                Assert.That(match.Groups[2].Value, Is.EqualTo(version));
+            }
+        }
+
+        [Test]
+        [TestCase(@"[HorribleSubs] Kuroko's Basketball 3 - 51 [1080p].mkv", "51")]
+        [TestCase(@"[HorribleSubs] Kuroko's Basketball 3 - 75 [1080p].mkv", "75")]
+        public void HorribleSubs_Kurokos_Basketball_3(string input, string episode)
+        {
+            var match = Regex.Match(input, @"\[HorribleSubs\] Kuroko's Basketball 3 - (\d+) \[1080p\].mkv",
+                RegexOptions.IgnoreCase);
+
+            Assert.That(match.Success, Is.True);
+
+            foreach (var @group in match.Groups)
+            {
+                Console.WriteLine(@group);
+            }
+
+            Assert.That(match.Groups.Count, Is.EqualTo(2));
+            Assert.That(match.Groups[1].Value, Is.EqualTo(episode));
+        }
+
+        [Test]
+        [TestCase(@"[Commie] Log Horizon - 06 [53AF6698].mkv", "06", "53AF6698")]
+        [TestCase(@"[Commie] Log Horizon - 21 [62388659].mkv", "21", "62388659")]
+        public void Commie_Log_Horizon(string input, string episode, string crc)
+        {
+            var match = Regex.Match(input, @"\[Commie\] Log Horizon - (\d+) \[([A-F0-9]{8})\].mkv",
+                RegexOptions.IgnoreCase);
+
+            Assert.That(match.Success, Is.True);
+
+            foreach (var @group in match.Groups)
+            {
+                Console.WriteLine(@group);
+            }
+
+            Assert.That(match.Groups.Count, Is.EqualTo(3));
+            Assert.That(match.Groups[1].Value, Is.EqualTo(episode));
+            Assert.That(match.Groups[2].Value, Is.EqualTo(crc));
+        }
+    }
+}
---
+++ src/AnimeXdcc.Core/Components/Filters/RegexService.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AnimeXdcc.Core.Components.Filters
+{
+    public class RegexService
+    {
+        private readonly string _pattern;
+
+        public RegexService(string pattern)
+        {
+            _pattern = pattern;
+        }
+
+        public EpisodeDetail ParseFileName(string fileName)
+        {
+            var match = Regex.Match(fileName, _pattern, RegexOptions.IgnoreCase);
+
+            if (!match.Success || MatchesOutOfRange(match.Groups.Count))
+            {
+                return null;
+            }
+
+            var episode = long.Parse(match.Groups[1].Value);
+            var version = match.Groups.Count == 3 ? long.Parse(match.Groups[2].Value) : 1;
+
+            return new EpisodeDetail(fileName, episode, version);
+        }
+
+        private static bool MatchesOutOfRange(int count)
+        {
+            return count > 3 || count < 2;
+        }
+    }
+
+    public class EpisodeDetail
+    {
+        public EpisodeDetail(string fullName, long episode, long version)
+        {
+            FullName = fullName;
+            Episode = episode;
+            Version = version;
+        }
+
+        public string FullName { get; private set; }
+        public long Episode { get; private set; }
+        public long Version { get; private set; }
+    }
+}
