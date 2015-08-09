9b763cea314ae7a7565f6b078f2b3de465d5ef47
Ahmed Agabani <agabani@live.com>
2015-08-09 20:39:48 +0100

    Added regex pattern builder

---
+++ src/AnimeXdcc.Core.Tests/Components/Filters/RegexPatternBuilderTests.cs
@@ -0,0 +1,49 @@
+﻿using AnimeXdcc.Core.Components.Filters.Builders;
+using NUnit.Framework;
+
+namespace AnimeXdcc.Core.Tests.Components.Filters
+{
+    [TestFixture]
+    public class RegexPatternBuilderTests
+    {
+        [Test]
+        public void Static_with_episode_number()
+        {
+            var pattern = new RegexPatternBuilder()
+                .WithStatic("[HorribleSubs] Kuroko's Basketball 3 - ")
+                .CaptureEpisode()
+                .WithStatic(" [1080p].mkv")
+                .Build();
+
+            Assert.That(pattern, Is.EqualTo(@"\[HorribleSubs\] Kuroko's Basketball 3 - (\d+) \[1080p\].mkv"));
+        }
+
+        [Test]
+        public void Static_with_episode_number_and_crc()
+        {
+            var pattern = new RegexPatternBuilder()
+                .WithStatic("[Commie] Log Horizon - ")
+                .CaptureEpisode()
+                .WithStatic(" [")
+                .CaptureCrc()
+                .WithStatic("].mkv")
+                .Build();
+
+            Assert.That(pattern, Is.EqualTo(@"\[Commie\] Log Horizon - (\d+) \[([A-F0-9]{8})\].mkv"));
+        }
+
+        [Test]
+        public void Static_with_episode_number_and_version()
+        {
+            var pattern = new RegexPatternBuilder()
+                .WithStatic("[HorribleSubs] Fate Stay Night - Unlimited Blade Works - ")
+                .CaptureEpisode()
+                .CaptureVersion()
+                .WithStatic(" [1080p].mkv")
+                .Build();
+
+            Assert.That(pattern,
+                Is.EqualTo(@"\[HorribleSubs\] Fate Stay Night - Unlimited Blade Works - (\d+)v?(\d+)? \[1080p\].mkv"));
+        }
+    }
+}
---
+++ src/AnimeXdcc.Core/Components/Filters/Builders/RegexPatternBuilder.cs
@@ -0,0 +1,45 @@
+﻿namespace AnimeXdcc.Core.Components.Filters.Builders
+{
+    public class RegexPatternBuilder
+    {
+        private string _pattern;
+
+        public RegexPatternBuilder()
+        {
+            _pattern = string.Empty;
+        }
+
+        public RegexPatternBuilder WithStatic(string term)
+        {
+            _pattern += term
+                .Replace(@"[", @"\[").Replace(@"]", @"\]");
+
+            return this;
+        }
+
+        public RegexPatternBuilder CaptureEpisode()
+        {
+            _pattern += @"(\d+)";
+
+            return this;
+        }
+
+        public RegexPatternBuilder CaptureVersion()
+        {
+            _pattern += @"v?(\d+)?";
+
+            return this;
+        }
+
+        public RegexPatternBuilder CaptureCrc()
+        {
+            _pattern += @"([A-F0-9]{8})";
+            return this;
+        }
+
+        public string Build()
+        {
+            return _pattern;
+        }
+    }
+}
